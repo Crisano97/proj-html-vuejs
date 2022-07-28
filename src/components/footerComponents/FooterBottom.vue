bfa80934b45c73b49ee801671512fbc75c8e5d83
Crisano97 <97keb6@gmail.com>
2022-07-28 15:24:47 +0200

    fixing footer components indentation

--- src/components/footerComponents/FooterBottom.vue
+++ src/components/footerComponents/FooterBottom.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="footer-bottom">
-      <div class="copyright">
-        <span>
-           @ Copyright 2012 - 2020
-        </span>
-        <span> | Avada Theme by Theme fusion</span>
-        <span> | all Rights Reserved</span>
-        <span>|Powered by Wordpress</span>
-      </div>
-      <div class="return-top">
-        <a href="#header">
-          <i class="fa-solid fa-angle-up"></i>
-        </a>
+        <div class="copyright">
+            <span>
+                @ Copyright 2012 - 2020
+            </span>
+            <span>
+                | Avada Theme by Theme fusion
+            </span>
+            <span>
+                | all Rights Reserved
+            </span>
+            <span>
+                |Powered by Wordpress
+            </span>
+        </div>
+        <div class="return-top">
+            <a href="#header">
+                <i class="fa-solid fa-angle-up"></i>
+            </a>
       </div>
     </div>
 </template>
@@ -28,34 +34,34 @@ export default {
 
 
     div.footer-bottom{
-      height: 10vh;
-      background-color: $grayBgColor;
-      position: relative;
-      display: flex;
-      align-items: center;
+        height: 10vh;
+        background-color: $grayBgColor;
+        position: relative;
+        display: flex;
+        align-items: center;
 
-      div.copyright{
-        padding-left: 1.5rem;
-        
-        font-size: .7rem;
-        color: gray;
-      }
-
-      div.return-top{
-        width: 3rem;
-        height: 2rem;
-        background-color: $arrowButtonColor;
-        position: absolute;
-        bottom: 0;
-        right: 5rem;
-        text-align: center;
-        line-height: 2rem;
-        cursor: pointer;
-
-        a{
-          text-decoration: none;
-          color: white;
+        div.copyright{
+            padding-left: 1.5rem;
+            
+            font-size: .7rem;
+            color: gray;
         }
-      }
+
+        div.return-top{
+            width: 3rem;
+            height: 2rem;
+            background-color: $arrowButtonColor;
+            position: absolute;
+            bottom: 0;
+            right: 5rem;
+            text-align: center;
+            line-height: 2rem;
+            cursor: pointer;
+
+            a{
+            text-decoration: none;
+            color: white;
+            }
+        }
     }
 </style>
